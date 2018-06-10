d5d0661e40285b9c3af8294dd3f7cc64f219490b
CrazyMorkovko <reginasakhapova@yandex.ru>
2018-06-11 00:03:13 +0300

    completed catalog page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-console.log('Hello, World!');
+const jQuery = require('jquery');
+window.$ = jQuery;
+
+$(document).on('click', '.catalog-menu-expand', function (event) {
+    $(this).parent().toggleClass('active');
+});
