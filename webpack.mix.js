f5d7ec1e8596d209ea1fc29724a2905c044cb06e
CrazyMorkovko <reginasakhapova@yandex.ru>
2018-06-10 20:03:36 +0300

    auto reload and local web server

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,13 @@
 let mix = require('laravel-mix');
 
+mix.browserSync({
+    proxy: false,
+    server: true,
+    files: [
+        'scripts/app.js',
+        'css/app.css',
+        '*.html']
+});
+
 mix.js('js/app.js', 'scripts')
     .sass('sass/app.scss', 'css');
